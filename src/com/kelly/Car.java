2c45246ce144f5d63c9f4a68b6ddafdea9ce7e9d
Kelly Douglass <kdouglass@gmail.com>
2017-11-12 14:02:44 -0600

    added Car and Vehicle classes

---
+++ src/com/kelly/Car.java
@@ -0,0 +1,15 @@
+package com.kelly;
+
+/**
+ * Created by kdoug on 11/12/2017.
+ */
+public class Car extends Vehicle {
+    private int doors;
+    private int engineCapacity;
+
+    public Car(String name, int doors, int engineCapacity) {
+        super(name);
+        this.doors = doors;
+        this.engineCapacity = engineCapacity;
+    }
+}
---
+++ src/com/kelly/Vehicle.java
@@ -0,0 +1,12 @@
+package com.kelly;
+
+/**
+ * Created by kdoug on 11/12/2017.
+ */
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
